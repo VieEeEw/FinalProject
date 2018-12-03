b14a552d491005ad661bd12c9e5ace720bce782f
Phillip Liu <liu0318yifei@163.com>
2018-12-03 17:11:09 -0600

    Some simple & small revisions.

--- app/src/main/java/com/uiuc/phillip/finalprojectcrawler/MainActivity.java
+++ app/src/main/java/com/uiuc/phillip/finalprojectcrawler/MainActivity.java
@@ -1,7 +1,16 @@
 package com.uiuc.phillip.finalprojectcrawler;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById (R.id.btnDoMagic);
+        btn.setOnClickListener(new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyApp", "This is a magic log message!");
+                Toast.makeText(getApplicationContext (), "It is magic!", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
--- app/src/main/java/com/uiuc/phillip/finalprojectcrawler/Parser.java
+++ app/src/main/java/com/uiuc/phillip/finalprojectcrawler/Parser.java
@@ -1,5 +1,8 @@
 package com.uiuc.phillip.finalprojectcrawler;
 
+import android.nfc.Tag;
+import android.util.Log;
+
 import org.jsoup.Jsoup;
 import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
@@ -17,47 +20,87 @@ public class Parser {
     public static final String urlBase = "https://courses.illinois.edu/schedule/2019/spring/";
     private String urlToParse;
 
+    private ArrayList<String[]> stored;
+
     public Parser() {
         urlToParse = "https://courses.illinois.edu/schedule/2019/spring/MATH/441";
     }
+
     Parser(String courseName, String courseNumber) {
         urlToParse = urlBase + courseName + "/" + courseNumber;
     }
-    public ArrayList<String> parseForCrn() {
+
+    public ArrayList<String[]> parseForCrn() {
         Document doc;
-        try{
-            doc = Jsoup.connect(urlToParse).get();
-        } catch(IOException e) {
-            e.printStackTrace();
+        try {
+            doc = Jsoup.connect (urlToParse).get ();
+        } catch (IOException e) {
+            e.printStackTrace ();
             return null;
         }
-        Elements scripts = doc.select("script");
+        Elements scripts = doc.select ("script");
         String useful = null;
-        for (int i = 0; i < scripts.size(); i++) {
-            Element script = scripts.get(i);
-            if (script.data().contains("crn")) {
-                useful = script.data();
+        for (int i = 0; i < scripts.size (); i++) {
+            Element script = scripts.get (i);
+            if (script.data ().contains ("crn")) {
+                useful = script.data ();
                 break;
             }
         }
         if (useful == null) {
             return null;
         }
-        Pattern pattern = Pattern.compile ("crn.+?([0-9]+)");
-        Matcher matcher1 = pattern.matcher (useful);
-        String name = null;
-        if (matcher1.find()) {
-            name = matcher1.group(1);
+        ArrayList<String> toUse = new ArrayList<> ();
+        for (String str: useful.split("crn...")) {
+            if (str.contains("credit")) {
+                for (String str1: str.split("...credit")) {
+                    if (str1.contains("availability")) {
+                        toUse.add (str1);
+                    }
+                }
+            }
         }
-
-        /*
-
-        */
+        if (toUse.size() == 0) {
+            return null;
+        }
+        String[] aval = new String[toUse.size()];
+        String[] crns = new String[toUse.size()];
+        int i = 0;
+        for (String str: toUse) {
+            aval[i] = str.split ("availability...")[1];
+            crns[i] = str.split ("...type")[0];
+            i++;
+        }
+        if (i != toUse.size()) {
+            return  null;
+        }
+        ArrayList<String[]> toReturn = new ArrayList<> (2);
+        toReturn.add(aval);
+        toReturn.add(crns);
+        stored = toReturn;
+        return toReturn;
+    }
+    public String[] getCrns() {
+        return stored.get (1);
+    }
+    public String[] getAval() {
+        return stored.get (0);
+    }
 }
 
 class Test {
-    public static void main(String[] args) {
-        Parser p = new Parser();
-        System.out.println(Arrays.toString(p.parseForCrn().toArray()));
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            Parser p = new Parser ();
+            ArrayList<String[]> store = p.parseForCrn ();
+            System.out.println (Arrays.deepToString (store.toArray ()));
+            String[] avl = store.get (0);
+            String[] crns = store.get (1);
+            for (int i = 0; i < avl.length; i++) {
+                System.out.println ("error");
+                System.out.println (avl[i] + " : " + crns[i]);
+            }
+            Thread.sleep (3000);
+        }
     }
 }
